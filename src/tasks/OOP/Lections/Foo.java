7ce5b04a050dc064e8eedf3921c2fb45335d0b17
DudenVal <valerapro33@yandex.ru>
2023-04-24 21:23:21 +0300

    Сделал 2 дз по ооп!

---
+++ src/tasks/OOP/Lections/Foo.java
@@ -0,0 +1,32 @@
+package tasks.OOP.Lections;
+class Doo extends Foo {
+    public void M(){
+        Foo.count =123;
+    }
+}
+public class Foo {
+    public Integer value;
+
+    protected static Integer count;
+
+    public static Integer getCount() {
+        return count;
+    }
+
+    static {
+        count = 0;
+    }
+
+    public Foo(){
+        count++;
+    }
+
+    public void printCount(){
+        System.out.println(count);
+    }
+
+    @Override
+    public String toString() {
+        return value.toString();
+    }
+}
---
+++ src/tasks/OOP/Seminar2/task1/Obstacle.java
@@ -0,0 +1,17 @@
+package tasks.OOP.Seminar2.task1;
+
+public interface Obstacle {
+    /**
+     * Длина препятсивия
+     * @return
+     */
+    int getLength();
+
+    /**
+     * Высота препятсивия
+     * @return
+     */
+
+    int getHeight();
+
+}
